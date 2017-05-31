b010a6b1205d76eae4930c03db7d3f2473f51a0b
Jung YunHo <saijdu2198@gmail.com>
2017-05-30 22:01:01 -0700

    Dynamic Post Query in post_list view

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
     context = {
-        'title': 'POST_LIST TITLE',
+        'title': 'SEOUL TIMES',
         'posts': posts
     }
     return render(request, 'blog/post_list.html', context=context)
