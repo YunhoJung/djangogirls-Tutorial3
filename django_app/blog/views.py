63561a0db7f7cf6f47d3f2abb5cd207eb2b70738
Jung YunHo <saijdu2198@gmail.com>
2017-05-30 21:08:42 -0700

    set STATIC_DIR to STATICFILES_DIRS, use static tag at post_list.html

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    context = {
+        'title': 'POST_LIST TITLE',
+        'posts': posts
+    }
+    return render(request, 'blog/post_list.html', context=context)
+
